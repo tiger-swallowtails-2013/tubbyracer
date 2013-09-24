f9d4a302124edbad6e5c88d07854d6168877e8b0
dbc-warrior <apprentice@devbootcamp.com>
2013-09-23 20:05:46 -0700

    final

--- public/javascripts/typerace.js
+++ public/javascripts/typerace.js
@@ -1,4 +1,5 @@
 function typeRacer(container) {
+  // alert(container.getElementByName('gameArea'))
   var start = container.children.namedItem('gameArea');
   var start_time = 0;
   var finish = container.children.namedItem('finishedRacing');
@@ -11,6 +12,8 @@ function typeRacer(container) {
     finish_time = new Date();
     // console.log("finish:" + finish_time);
     total_time = finish_time - start_time;
-    results.innerHTML = "Congrats! You took" + " " + (total_time/600) + " " + "seconds.";
+    var message = "Congrats! You took" + " " + (total_time/600) + " " + "seconds.";
+    alert(message);
+    results.innerHTML = message;
   };
 }
