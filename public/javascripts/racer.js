3b0f249977c625ab54299178d3eb0a91acb3af36
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-09-25 12:01:08 -0700

    testing matching text

--- public/javascripts/racer.js
+++ public/javascripts/racer.js
@@ -1,3 +1,21 @@
 function typeRacer(container){
-  console.log('executed');
+
 };
+
+function getWhatUserIsSupposedToType() {
+  return document.querySelector('.gameData').innerText;
+}
+
+function getWhatUserDoneDidSay() {
+  return document.querySelector('.gameArea').value;
+}
+
+function compareUserInputToExpectedInput(gameText, userText) {
+  return gameText.slice(0, userText.length) == userText
+}
+
+
+
+  //  String.prototype.matchData = function (str){
+  //   return this.slice(0, str.length) == str;
+
--- spec/racerSpec.js
+++ spec/racerSpec.js
@@ -1,5 +1,5 @@
 describe("typeracer", function(){
-  var game, gameData, gameArea;
+  var game, gameData, gameArea, gameText, userCorrectInput, userWrongInput;
 
   function appendToDom(element, className, parent){
     parent = parent || document.body;
@@ -15,11 +15,43 @@ describe("typeracer", function(){
     gameData = appendToDom('p', 'gameData', game);
     gameArea = appendToDom('textarea', 'gameArea', game);
 
+    gameText = gameData.innerText = "This is the game text"
+    userWrongInput =gameArea.innerText = "This is not supposed to match"
+    userCorrectInput = gameArea.innerText = "This is the game text"
     typeRacer(document);
   });
 
   // afterEach(function){
   //   document.body.removeChild(game);
   // };
+
+  describe("Getting data", function() {
+    it("should get the gameText from the DOM", function(){
+      var text = getWhatUserIsSupposedToType()
+      expect(text).toEqual(gameText);
+
+    })
+
+    it("should get the userCorrectInput from the DOM", function(){
+      var some_text = getWhatUserDoneDidSay()
+      expect(some_text).toEqual(userCorrectInput);
+    })
+
+  })
+
+  describe("Compare user input to what user is supposed to type", function() {
+
+    it("return true if the user input matches the original string", function() {
+
+      expect(compareUserInputToExpectedInput(gameText, userCorrectInput)).toBeTruthy()
+    });
+
+    it("returns false if the user input does not match the original string", function(){
+      expect(compareUserInputToExpectedInput(gameText, userWrongInput)).toBeFalsy()
+
+    });
+
+  });
+
 
 });
