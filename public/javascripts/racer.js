ff5734be16dd306d74b5d512e5951da23e03d48d
DBC Apprentice <apprentice@devbootcamp.com>
2013-09-25 16:50:44 -0700

    adds timer object, fully tested

--- public/javascripts/racer.js
+++ public/javascripts/racer.js
@@ -1,9 +1,30 @@
+var timer = {
+  tick: -3,
+  stop: false,
+  state: function(){
+    if(stop)
+      return 'after'
+    else if(this.tick < 0){
+      return 'before'
+    }
+    else    {
+      return 'during'
+    }
+  },
+  count: function(){
+    this.tick++;
+  },
+  end: function(){
+    this.stop = true;
+  },
+  reboot: function(){
+    this.tick = -3;
+    this.state = 'before';
+    this.stop = false;
+  }
+}
+
 function typeRacer(container){
   var gameArea = container.querySelector("#gameArea");
-
-  initializeGame = function(){
-      gameArea.disabled = true;
-  }
-
-  initializeGame();
+  gameArea.disabled = true;
 };
