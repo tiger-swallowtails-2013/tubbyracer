c752f0ae5b0494f7f2ed17354afea5be14038e2b
DBC Apprentice <apprentice@devbootcamp.com>
2013-09-25 11:20:39 -0700

    adds timer tests

--- spec/racerSpec.js
+++ spec/racerSpec.js
@@ -1,10 +1,10 @@
 describe("typeracer", function(){
   var game, gameData, gameArea;
 
-  function appendToDom(element, className, parent){
+  function appendToDom(element, idName, parent){
     parent = parent || document.body;
     var element = document.createElement(element);
-    element.classList.add(className);
+    element.id = idName
     parent.appendChild(element);
 
     return element;
@@ -14,12 +14,30 @@ describe("typeracer", function(){
     game = appendToDom('div', 'game');
     gameData = appendToDom('p', 'gameData', game);
     gameArea = appendToDom('textarea', 'gameArea', game);
+    startButton = appendToDom('button', 'startButton', game);
 
-    typeRacer(document);
+    typeRacer(game);
   });
 
   // afterEach(function){
   //   document.body.removeChild(game);
   // };
+  describe("Timer", function(){
+    it("should prevent the user from typing input before the start", function(){
+      expect(gameArea.disabled).toBeTruthy();
+    });
+    describe("Start", function(){
+      it("should log a start time", function(){
 
+      });
+      it("should allow the user to type input", function(){
+
+      });
+    });
+    describe("Finish", function(){
+      it("should log a finish time", function(){
+
+      });
+    });
+  })
 });
