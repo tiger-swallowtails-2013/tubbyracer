bc02e8b1d19f8ba6ac6cc57bc347f71c7c91181c
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-09-26 16:01:06 -0700

    fix wordTracker to run in jasmine

--- spec/wordTrackerSpec.js
+++ spec/wordTrackerSpec.js
@@ -3,33 +3,33 @@ describe("word tracker", function(){
   wordTracker.typedWord = 'wo'
 
   it("should know the correct word", function(){
-    expect(wordTracker.correctWord == "wood");
+    expect(wordTracker.correctWord).toEqual("wood");
   });
   it("should know the word being typed", function(){
-    expect(wordTracker.typedWord == "wo");
+    expect(wordTracker.typedWord).toEqual("wo");
   });
   it("should add a letter to the word being typed", function(){
     wordTracker.addLetter('o');
-    expect(wordTracker.typedWord == 'woo')
+    expect(wordTracker.typedWord).toEqual('woo');
   });
   it("should remove a letter from the word being typed", function(){
     wordTracker.removeLetter();
-    expect(wordTracker.typedWord == 'wo');
+    expect(wordTracker.typedWord).toEqual('wo');
   });
   describe("word is incomplete", function(){
     it("should know if the word is incomplete", function(){
-      expect(wordTracker.finished == false);
+      expect(wordTracker.finished()).toBeFalsy();
     });
     it("should know if word is on track", function(){
-      expect(wordTracker.onTrack == true);
+      expect(wordTracker.onTrack()).toBeTruthy();
     });
     it("should know if word is off track", function(){
       wordTracker.addLetter('k');
-      expect(wordTracker.onTrack == false);
+      expect(wordTracker.onTrack()).toBeFalsy();
     });
   });
   it("should know if the word is complete", function(){
     wordTracker.typedWord = 'wood';
-    expect(wordTracker.finished == true)
+    expect(wordTracker.finished()).toBeTruthy();
   });
 });
