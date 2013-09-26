0e0c50e092fdcaae9f97dd44104f028d79070977
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-09-26 15:24:06 -0700

    adds game and word object specs

--- public/javascripts/racer.js
+++ public/javascripts/racer.js
@@ -1,20 +1,26 @@
 function typeRacer(container) {
-  var gameArea = container.querySelector('.gameArea')
+  var gameArea = container.querySelector('.mainArea')
   gameArea.addEventListener("keydown", function(e) {
     handleKeyPress(e);
   });
+
+  var buttonClick = container.querySelector('.mainStart')
+  buttonClick.addEventListener("click", function(e) {
+    game.handleButtonClick(e);
+  })
 }
 
 var timer = {
   tick: -3,
   stop: false,
   state: function(){
-    if(stop)
+    if(stop){
       return 'after'
+    }
     else if(this.tick < 0){
       return 'before'
     }
-    else    {
+    else{
       return 'during'
     }
   },
@@ -31,29 +37,56 @@ var timer = {
   }
 }
 
-
-
-function getWhatUserIsSupposedToType() {
-  return document.querySelector('.gameData').innerText;
+var wordTracker = {
+  correctWord: "",
+  typedWord: "",
+  addLetter: function(letter){
+    this.typedWord+=letter;
+  },
+  removeLetter: function(){
+    this.typedWord = this.typedWord.slice(0, (this.typedWord.length-1))
+  },
+  finished: function(){
+    return (this.correctWord == this.typedWord)
+  },
+  onTrack: function(){
+    var test = new RegExp(typedWord)
+    if(correctWord.match(test)){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
 }
 
-function getWhatUserDoneDidSay() {
-  return document.querySelector('.gameArea').value;
+var game = {
 
+  begin: function() {
+    timer.reboot();
+  },
+  getWhatUserIsSupposedToType: function() {
+    return document.querySelector('.mainData').innerText;
+  },
+  getWhatUserDoneDidSay: function() {
+    return document.querySelector('.mainArea').value;
+  },
+  compareUserInputToExpectedInput: function(gameText, userText) {
+    return gameText.slice(0, userText.length) == userText
+  },
+  handleKeyPress: function(evnt) {
+    var userInput = getWhatUserDoneDidSay()
+    var gameText = getWhatUserIsSupposedToType()
+    return compareUserInputToExpectedInput(gameText,userInput)
+  },
+  handleButtonClick: function(evnt) {
+    this.begin()
+  },
+  currentWord: function() {
+    var gameData = this.getWhatUserIsSupposedToType()
+    return gameData.split(" ")[0]
+  }
 }
-
-function compareUserInputToExpectedInput(gameText, userText) {
-  return gameText.slice(0, userText.length) == userText
-}
-
-function handleKeyPress(evnt) {
-
-  var userInput = getWhatUserDoneDidSay()
-  var gameText = getWhatUserDoneDidSay()
-  return compareUserInputToExpectedInput(gameText,userInput)
-}
-
-
   //  String.prototype.matchData = function (str){
   //   return this.slice(0, str.length) == str;
 
